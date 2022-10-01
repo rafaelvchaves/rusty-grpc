cc9a36f97aabd14a9f4fb92e19a98578483bd6e1
Rafael Chaves <rafaelvchaves@gmail.com>
2022-10-01 11:45:53 -0400

    Applied reformatting

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,8 @@
 use tonic::{transport::Server, Request, Response, Status};
-use voting::{VotingRequest, VotingResponse, voting_server::{Voting, VotingServer}};
+use voting::{
+    voting_server::{Voting, VotingServer},
+    VotingRequest, VotingResponse,
+};
 
 pub mod voting {
     tonic::include_proto!("voting");
@@ -10,28 +13,34 @@ pub struct VotingService {}
 
 #[tonic::async_trait]
 impl Voting for VotingService {
-  async fn vote(&self, request: Request<VotingRequest>) -> Result<Response<VotingResponse>, Status> {
-    let r = request.into_inner();
-    match r.vote {
-      0 => Ok(Response::new(voting::VotingResponse { confirmation: { 
-        format!("Happy to confirm that you upvoted for {}", r.url)
-      }})),
-      1 => Ok(Response::new(voting::VotingResponse { confirmation: { 
-        format!("Confirmation that you downvoted for {}", r.url)
-      }})), 
-      _ => Err(Status::new(tonic::Code::OutOfRange, "Invalid vote provided"))
+    async fn vote(
+        &self,
+        request: Request<VotingRequest>,
+    ) -> Result<Response<VotingResponse>, Status> {
+        let r = request.into_inner();
+        match r.vote {
+            0 => Ok(Response::new(voting::VotingResponse {
+                confirmation: { format!("Happy to confirm that you upvoted for {}", r.url) },
+            })),
+            1 => Ok(Response::new(voting::VotingResponse {
+                confirmation: { format!("Confirmation that you downvoted for {}", r.url) },
+            })),
+            _ => Err(Status::new(
+                tonic::Code::OutOfRange,
+                "Invalid vote provided",
+            )),
+        }
     }
-  }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let address = "[::1]:8080".parse().unwrap();
-  let voting_service = VotingService::default();
+    let address = "[::1]:8080".parse().unwrap();
+    let voting_service = VotingService::default();
 
-  Server::builder().add_service(VotingServer::new(voting_service))
-    .serve(address)
-    .await?;
-  Ok(())
-     
+    Server::builder()
+        .add_service(VotingServer::new(voting_service))
+        .serve(address)
+        .await?;
+    Ok(())
 }
